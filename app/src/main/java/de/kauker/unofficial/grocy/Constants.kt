d8f88954025970cc85ee880c30cb0cdce89065ac
aimok04 <aimo.kauker@gmail.com>
2023-03-07 23:04:01 +0100

    feat(app): added install url for google play

--- app/src/main/java/de/kauker/unofficial/grocy/Constants.kt
+++ app/src/main/java/de/kauker/unofficial/grocy/Constants.kt
@@ -1,4 +1,7 @@
 package de.kauker.unofficial.grocy
 
-const val INSTALL_COMPANION_APP_URL: String =
-    "https://github.com/aimok04/grocy-for-wear-os/releases/latest"
+/* github */
+const val INSTALL_COMPANION_APP_URL: String = "https://github.com/aimok04/grocy-for-wear-os/releases/latest"
+
+/* google play */
+//const val INSTALL_COMPANION_APP_URL: String = "https://play.google.com/store/apps/details?id=de.kauker.unofficial.grocy"
